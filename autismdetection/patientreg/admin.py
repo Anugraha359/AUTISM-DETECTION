55b366b853e252ac4a8c297668138025d8363b18
Anugraha359 <anugrahak2001@gmail.com>
2025-06-08 07:52:57 +0530

    Delete autismdetection directory
    Delete

--- autismdetection/patientreg/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from patientreg import models
-admin.site.register(models.PregnancyDetails)
-admin.site.register(models.BirthMilestones)
-admin.site.register(models.Patient)
